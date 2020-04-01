319a9468acff5db1518958786ca3201e43269dd4
eson <474420502@qq.com>
2020-04-02 02:18:14 +0800

    TODO name_creator

--- gtest/creator_test.cpp
+++ gtest/creator_test.cpp
@@ -3,22 +3,55 @@
 #include "creator.hpp"
 #include <gtest/gtest.h>
 
-
-TEST(creator_test, number_creator) {
+TEST(creator_test, number_creator)
+{
     ulong n;
 
     auto create = creator<ulong>::make_number_creator(0, 1000);
-    n = create->create();
 
-    ASSERT_TRUE(n == 1) << n;
+    for (int i = 0; i < 1000; i++)
+    {
+        n = create->create();
+        ASSERT_LE(n, 1000) << n;
+    }
 }
 
-TEST(creator_test, real_creator) {
+TEST(creator_test, real_creator)
+{
     double n;
     auto create = creator<double>::make_real_creator(0.0, 1.0);
     n = create->create();
-    ASSERT_TRUE(n == 1.0) << n;
+    for (int i = 0; i < 1000; i++)
+    {
+        n = create->create();
+        ASSERT_LT(n, 1.0) << n;
+    }
+
+    create = creator<double>::make_real_creator(0.0, 2.0);
+    n = create->create();
+    for (int i = 0; i < 1000; i++)
+    {
+        n = create->create();
+        ASSERT_LT(n, 2.0) << n;
+    }
+
+    auto create2 = creator<float>::make_real_creator(2.0, 4.0);
+    for (int i = 0; i < 1000; i++)
+    {
+        n = create2->create();
+        ASSERT_LT(n, 4.0) << n;
+        ASSERT_GE(n, 2.0) << n;
+    }
+
 }
 
-
-
+TEST(creator_test, name_creator) {
+    wstring n;
+    auto create = creator<wstring>::make_name_creator();
+    n = create->create();
+    for (int i = 0; i < 1000; i++)
+    {
+        n = create->create();
+        ASSERT_EQ(n, L"1") << n;
+    }
+}
---
+++ src/initializer.hpp
@@ -0,0 +1,23 @@
+
+#ifndef __INITIALIZER__H__
+#define __INITIALIZER__H__
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+// using std::vector;
+// using std::wstring;
+
+// class initializer {
+// public:
+//     initializer() {
+        
+//     };
+
+//     vector<wstring> name_creator() {
+
+//     }
+// };
+
+#endif __INITIALIZER__H__
